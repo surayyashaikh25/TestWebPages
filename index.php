c9ff289721f19831e187f39ed10feb1c8907a4a4
Surayya Shaikh <70680662+surayyashaikh25@users.noreply.github.com>
2021-06-24 10:58:49 +0530

    webpage

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+  <body style="background-color:#ccffff;">
+<center>
+<h1 style="color:#66ff33; background-color:#000066; height:80px">"Welcome from AWS-EC2...<i> configured with Terraform</i>." !!!</h1>
+  <h2 style="color:#ff0066;" > IP address of instance is,</h2>
+</center>
+<br>
+<pre>
+<?php
+echo "<h2 style='color: purple;'>". `/usr/sbin/ifconfig`. "</h2>";
+?>
+</pre>
+  </body>
+</html>
